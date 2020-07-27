872da3dcb2ab8a9277d2887ed0a9568235e36e2b
LuongDzai <vuxuanluong12345@gmail.com>
2020-07-27 14:53:26 +0700

    tỉmer su dung useEffect to clearInterval

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from "react";
 export default function Timer() {
   const [counter, setCounter] = useState(10);
 
-  useEffect(() => {
-    // const timer =
-    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-    // return () => clearTimeout(timer);
-  }, [counter]);
+  // useEffect(() => {
+  //   // const timer =
+  //   counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  //   // return () => clearTimeout(timer);
+  // }, [counter]);
 
   // // First Attempts
   // setInterval(() => setCounter(counter - 1), 1000);
@@ -26,12 +26,14 @@ export default function Timer() {
   //     }, 1000);
   // }, []);
 
-  // // Third Attempts
-  // useEffect(() => {
-  //   const timer =
-  //     counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-  //   return () => clearInterval(timer);
-  // }, [counter]);
+  // Third Attempts
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (counter > 0)
+      timer = setInterval(() => setCounter(counter - 1), 1000);
+    // nếu counter <=0 thì truyền vào id cái timer để huỷ và chuyển sang việc ko đếm n
+    return () => clearInterval(timer);
+  }, [counter]);
 
   return <div className="timer">Countdown: {counter}</div>;
 }
